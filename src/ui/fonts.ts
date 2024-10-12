b5873c0e911d094e883df0aac6896654f630fe5a
beugene <eugene.bugor@gmail.com>
2024-10-12 18:43:38 +0200

    limited font weights load

--- src/ui/fonts.ts
+++ src/ui/fonts.ts
@@ -2,10 +2,12 @@ import { Inter, Lora } from 'next/font/google';
 
 export const lora = Lora({
   subsets: ['latin'],
+  weight: ['400', '500', '700'],
   variable: '--font-lora',
 });
 
 export const inter = Inter({
   subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '800'],
   variable: '--font-inter',
 });
