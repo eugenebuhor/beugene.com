a567ad05eee73be01bf4181bea1ce40d78f7fb5f
beugene <eugene.bugor@gmail.com>
2024-10-16 18:38:50 +0200

    vercel public baseUrl

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,8 +1,8 @@
 const getBaseUrl = () => {
   if (process.env.NODE_ENV === 'development') {
-    return `http://${process.env.NEXT_PUBLIC_URL}`;
+    return `http://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   } else {
-    return `https://${process.env.NEXT_PUBLIC_URL}`;
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   }
 };
 
