22ab7994322d7d4cd57a2c62ff7a535103a2a8e2
beugene <eugene.bugor@gmail.com>
2024-10-31 22:42:08 +0100

    added favicon and updated logo; removed page loaders to improve SEO

--- src/ui/common/Header.tsx
+++ src/ui/common/Header.tsx
@@ -1,21 +1,15 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import { LayoutSection } from '@/ui/common/Layouts';
 import styles from '@/ui/common/Header.module.css';
 import Typography from '@/ui/common/Typography';
+import LogoImage from '@/ui/common/LogoImage';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <LayoutSection className={styles.layoutSection} component="nav">
-        <Link
-          href="/"
-          className={styles.nameLogoWrapper}
-          rel="index"
-          aria-label="homepage"
-          title="Yevhenii Buhor"
-        >
-          <Image draggable={false} src="/name-logo-24.svg" alt="Yevhenii Buhor" fill priority />
+        <Link href="/" className={styles.nameLogoWrapper} rel="index" aria-label="homepage">
+          <LogoImage />
         </Link>
         <Link href="/about" prefetch>
           <Typography variant="body2" color="text-primary" fontFamily="subtitle">
