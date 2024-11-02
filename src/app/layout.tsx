256b3b01de4f5c4fb35a9c1fbe8d1e18f46cd4c9
beugene <eugene.bugor@gmail.com>
2024-11-02 17:32:28 +0100

    added vercel analytics and speed insights tracker (#33)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from 'react';
 import type { Metadata } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
 import * as fonts from '@/ui/fonts';
 import Header from '@/ui/common/Header';
@@ -39,6 +41,8 @@ const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
         <link rel="preload" href="/logo-light.svg" as="image" />
       </head>
       <body>
+        <Analytics />
+        <SpeedInsights />
         <ScrollHandler />
         <NextThemeProvider defaultTheme="system" disableTransitionOnChange enableColorScheme>
           <Header />
